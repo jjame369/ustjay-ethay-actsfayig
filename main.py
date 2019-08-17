60c69de8b8dd8a6fe5ed412e67aa159315342b99
Justin Jameson <jjameson@uw.edu>
2019-08-16 21:29:01 -0700

    'updating'

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ def get_fact():
 def post_pig_lat(rand_post):
     response = requests.post("http://hidden-journey-62459.herokuapp.com/piglatinize/",
                              data={'input_text': rand_post}, allow_redirects=False)
-    soup = BeautifulSoup(response.content, "html.parser")
-    content = soup.find_all("a", href="/")
-    return content  # response.text
+    # soup = BeautifulSoup(response.content, "html.parser")
+    # content = soup.find_all("a", href="/")
+    # return content  # response.text
+    return response.text
 
 
 @app.route('/')
